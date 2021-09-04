d402107d9b72b12202553098ab253fa725394666
SumanRBangera <70790498+SumanRBangera@users.noreply.github.com>
2021-09-04 22:00:45 +0530

    Update Covid19_Desktop_Notifier.py

--- Covid19_Desktop_Notifier.py
+++ Covid19_Desktop_Notifier.py
@@ -1,8 +1,9 @@
 #using webscrapping
 
-from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup as bs
-from win10toast import ToastNotifier
+from urllib.request import urlopen, Request    ##for fetching and opening url
+from bs4 import BeautifulSoup as bs         ##for pulling data
+from plyer import notification    ##notification
+import time
 
 header = {"User-Agent":"Mozilla"}
 req = Request("https://www.worldometers.info/coronavirus/country/india/",headers = header)
@@ -14,8 +15,13 @@ obj = bs(html,"html.parser")
 new_cases = obj.find("li",{"class":"news_li"}).strong.text.split()[0]
 death = list(obj.find("li",{"class":"news_li"}).strong.next_siblings)[1].text.split()[0]
 
-notifier = ToastNotifier()
-message = "New Cases - "+ new_cases+"\nDeath - "+death
-print(message)
+def notifyme(notify_title, newcases, deaths):
+    notification.notify(title = notify_title,
+                       message =  "New Cases - "+ newcases+"\nDeaths - "+deaths,
+                        timeout = 5)
+                        
 
-notifier.show_toast(title = "Covid-19 Update", msg = message, duration = 10, icon_path = None)
+while True:
+    notifyme("Covid-19 Update", new_cases, death)
+    time.sleep(10)
+
